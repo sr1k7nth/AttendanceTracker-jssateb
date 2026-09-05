5c0a80021f090d5760616b813313e305fc96c3c0
Srikanth <sr1k7nth@gmail.com>
2026-09-05 17:23:57 +0530

    fix: wire database.py to use settings.database_url from env vars

--- backend/api/database.py
+++ backend/api/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DB_URL = "postgresql://postgres:srikanth02@localhost/jss_attendance_tracker"
+from .config import settings
 
-engine = create_engine(SQLALCHEMY_DB_URL)
+engine = create_engine(settings.database_url)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
